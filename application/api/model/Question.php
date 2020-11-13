e6b3c136ce067b424460830ecd1ecd6d0070f8e0
root_mi <845414233@qq.com>
2020-11-13 16:53:47 +0800

    2020/11/13-mi

--- application/api/model/Question.php
+++ application/api/model/Question.php
@@ -8,8 +8,12 @@ class Question extends Model
       public $name="question";
       public $id;
       public function _initialize(){
-
+          if(!$this->id){
+              echo json_encode(['code'=>0,'data'=>'','msg'=>'缺少条件']);
+          }
          parent::_initialize();
       }
-      public function
+      public function get_question(){
+
+      }
 }
